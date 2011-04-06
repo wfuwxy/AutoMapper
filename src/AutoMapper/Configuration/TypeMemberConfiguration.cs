706e5dbabde6e72a042b4cd355338001c952d7fe
Jimmy Bogard <jimmy.bogard@gmail.com>
2011-04-05 21:35:33 -0500

    Map from configuration

--- src/AutoMapper/Configuration/TypeMemberConfiguration.cs
+++ src/AutoMapper/Configuration/TypeMemberConfiguration.cs
@@ -1,10 +1,11 @@
 using System;
-using System.Collections.Generic;
 
 namespace AutoMapper.Configuration
 {
     public class TypeMemberConfiguration<TSource>
     {
+        public IMapFromConfiguration SourceMemberConfiguration { get; private set; }
+
         public IMemberAccessor CreateAccessor()
         {
             return null;
@@ -12,11 +13,33 @@ namespace AutoMapper.Configuration
 
         public void Apply(PropertyMap propertyMap)
         {
+            if (SourceMemberConfiguration != null)
+                SourceMemberConfiguration.Apply(propertyMap);
         }
 
-        public void MapFrom(Func<TSource, object> sourceMember)
+        public void MapFrom<TDestination>(Func<TSource, TDestination> sourceMember)
         {
-            _propertyMapActions.Add(pm => pm.AssignCustomValueResolver(new DelegateBasedResolver<TSource>(sourceMember)));
+            SourceMemberConfiguration = new MapFromConfiguration<TDestination>(sourceMember);
+        }
+
+        private class MapFromConfiguration<TDestination> : IMapFromConfiguration
+        {
+            private readonly Func<TSource, TDestination> _sourceMember;
+
+            public MapFromConfiguration(Func<TSource, TDestination> sourceMember)
+            {
+                _sourceMember = sourceMember;
+            }
+
+            public void Apply(PropertyMap propertyMap)
+            {
+                propertyMap.AssignCustomValueResolver(new DelegateBasedResolver<TSource, TDestination>(_sourceMember));
+            }
+
+            public Delegate SourceMember
+            {
+                get { return _sourceMember; }
+            }
         }
     }
 }
